19376ef3c7d584f56ae8ac9d2f019e0b8b4c2f37
Clyde D'Souza <clyd94@gmail.com>
2025-11-08 01:40:51 +1300

    Add readline library to make chat interactive and infinite

--- index.ts
+++ index.ts
@@ -1,5 +1,25 @@
+import * as readline from "readline";
+
 function main() {
   console.log("Hello world");
+
+  const readlineClient = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> You: ",
+  });
+
+  readlineClient.prompt();
+
+  readlineClient.on("line", async (userMessage) => {
+    console.log("Received:", userMessage);
+    readlineClient.prompt();
+  });
+
+  readlineClient.on("close", () => {
+    console.log("Bye");
+    process.exit(0);
+  });
 }
 
 main();

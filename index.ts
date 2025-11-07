101680aba1f2b0bae259e676c8a1dfe99a35c01f
Clyde D'Souza <clyd94@gmail.com>
2025-11-08 12:35:13 +1300

    Add OpenAI client

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
+import { AzureOpenAI } from "openai";
 import * as readline from "readline";
 
 function main() {
   console.log("Hello world");
+
+  const endpoint = process.env.AZURE_OPENAI_ENDPOINT!;
+  const apiKey = process.env.AZURE_OPENAI_API_KEY!;
+  const apiVersion = process.env.AZURE_OPENAI_API_VERSION;
+  const deployment = process.env.AZURE_OPENAI_DEPLOYMENT_NAME!;
+
+  const openaiClient = new AzureOpenAI({
+    endpoint,
+    apiKey,
+    apiVersion,
+    deployment,
+  });
 
   const readlineClient = readline.createInterface({
     input: process.stdin,
